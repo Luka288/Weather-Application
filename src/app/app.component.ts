21363ad00c8f4097088816380fd96d1213b919c7
Luka <lukagaxokidze28@gmail.com>
2025-02-26 23:56:29 +0400

    ui corrections (hourly component)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
 import { NavigationComponent } from './features/navigation/navigation.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterOutlet, CommonModule, NavigationComponent],
+  imports: [RouterOutlet, RouterOutlet, NavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
