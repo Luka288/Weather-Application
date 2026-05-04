ef90a48d100d8b877f6e7fc4b5a6526b4a39cd5e
Luka <lukagaxokidze28@gmail.com>
2026-05-04 22:49:44 +0400

    ref: created new reusable component for location edge case, updated logic inside component

--- src/app/features/main/main.component.ts
+++ src/app/features/main/main.component.ts
@@ -1,26 +1,15 @@
-import { Component, computed, effect, inject, signal } from '@angular/core';
-import { catchError, EMPTY, filter, switchMap, tap } from 'rxjs';
+import { Component, computed, inject } from '@angular/core';
+import { catchError, EMPTY, filter, merge, switchMap } from 'rxjs';
 import { WeatherAPIService } from '../../core/services/weather-api.service';
-import {
-  hourlyRate,
-  WeatherResponse,
-} from '../../core/interfaces/weatherInterface';
 import { CommonModule } from '@angular/common';
-import { SearchWeatherService } from '../../core/services/search-weather.service';
-import { HeaderServiceService } from '../../core/services/header-service.service';
-import { NgxCubeLoaderComponent } from 'ngx-cube-loader';
-import { RoundTempPipe } from '../../core/pipes/round-temp.pipe';
 import { LocationService } from '../../core/services/location.service';
-import { conditions } from '../../core/consts/dynamic.backrounds';
 import { DynamicBgService } from '../../core/services/dynamic-bg.service';
 import { HourlyContainerComponent } from '../../shared/components/hourly-container/hourly-container.component';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
-import { ForecastCardComponent } from '../../shared/components/forecast-card/forecast-card.component';
+import { ReactiveFormsModule } from '@angular/forms';
 import { StateService } from '../../core/services/state.service';
-import { TrackWidthDirective } from '../../core/directives/track-width.directive';
-import { DateFormatPipe } from '../../core/pipes/date-format.pipe';
 import { WeatherDataComponent } from '../../shared/components/weather-data/weather-data.component';
 import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { SearchComponent } from '../../shared/components/search/search.component';
 import { currLocationInter } from '../../core/interfaces/currInterface';
 
 @Component({
@@ -28,43 +17,49 @@ import { currLocationInter } from '../../core/interfaces/currInterface';
   standalone: true,
   imports: [
     CommonModule,
-    NgxCubeLoaderComponent,
     HourlyContainerComponent,
     ReactiveFormsModule,
     WeatherDataComponent,
+    SearchComponent,
   ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
 })
 export default class MainComponent {
   private readonly weatherAPI = inject(WeatherAPIService);
-  private readonly searchWeather = inject(SearchWeatherService);
-  private readonly currService = inject(LocationService);
+  private readonly locationService = inject(LocationService);
   private readonly dynamicBg = inject(DynamicBgService);
 
-  private readonly headerBoolean = inject(HeaderServiceService);
   readonly stateService = inject(StateService);
-
-  // displayWeather = signal<WeatherResponse | null>(null);
-  // hourly = signal<hourlyRate[]>([]);
-
-  searchLocation = computed(() => this.weatherAPI.searchLocation());
 
   // To handle edge case when there is no user search value stored
   // Into localStorage searchBar computed method will toggle container
   // Which allows user to manually search its location
   searchBar = computed(() => this.weatherAPI.searchBar());
 
-  locationSearch = new FormControl('', { nonNullable: true });
+  searchLocation = computed(() => this.weatherAPI.searchLocation());
+
+  currentBackground = computed(() => {
+    const icon = this.weatherData()?.currentConditions.icon;
+    return this.dynamicBg.getVideoPath(icon);
+  });
 
   currDate = Date.now();
 
-  readonly location = toSignal(this.currService.getCurr());
+  readonly location = toSignal(this.locationService.getCurr());
 
   readonly weatherData = toSignal(
-    toObservable(this.stateService.searchValue).pipe(
-      filter((city): city is string => !!city),
-      switchMap((city) => this.weatherAPI.getWeather(city)),
+    merge(
+      toObservable(this.location),
+      toObservable(this.stateService.searchValue),
+    ).pipe(
+      filter((val): val is string | currLocationInter => !!val),
+
+      switchMap((val) => {
+        const cityQuery = typeof val === 'string' ? val : val.city;
+        return this.weatherAPI.getWeather(cityQuery);
+      }),
+
       catchError((e) => {
         console.error(e);
         return EMPTY;
@@ -72,86 +67,4 @@ export default class MainComponent {
     ),
     { initialValue: null },
   );
-
-  ngOnInit(): void {
-    this.location();
-
-    // this.getSearch();
-    // this.initWeather();
-    // this.updatingBooleans();
-  }
-
-  initWeather() {
-    const memory = localStorage.getItem('searchMemory');
-
-    // In case of there is user search query stored in localStorage
-    // Weather is loading from server, but in other case there is
-    // Container to make user to search location manually
-    if (memory) {
-      this.loadWeather(memory);
-    } else if (!memory) {
-      this.loadCurr();
-      // this.loadingScreen.set(true);
-      // this.searchBar.set(true);
-    }
-  }
-
-  updatingBooleans(): void {
-    this.weatherAPI.searchBar();
-
-    this.weatherAPI.searchLocation();
-  }
-
-  loadCurr() {
-    this.currService.getCurr().subscribe({
-      next: (res) => {
-        this.loadWeather(res.city);
-        // this.loadingScreen.set(false);
-      },
-      error: () => {
-        // this.searchBar.set(true);
-        // this.userLocationSearch.set(true);
-        // this.loadingScreen.set(false);
-      },
-    });
-  }
-
-  loadWeather(location: string) {
-    if (!location) {
-      // this.searchLocation.set(false);
-      // this.searchBar.set(true);
-      // this.loadingScreen.set(false);
-      // this.headerBoolean.isHeaderAvailable(false);
-      return;
-    }
-
-    this.weatherAPI.getWeather(location).subscribe({
-      next: (res) => {
-        // this.hourly.set(res.days[0].hours);
-        // this.displayWeather.set(res);
-        // this.headerBoolean.isHeaderAvailable(true);
-      },
-    });
-  }
-
-  getSearch() {
-    // this.searchWeather.searchValue$.subscribe((value) => {
-    //   if (value) {
-    //     this.activeLocation.set(value);
-    //   }
-    // });
-  }
-
-  setBg() {
-    return this.dynamicBg.getVideoPath(
-      this.weatherData()?.currentConditions.icon,
-    );
-  }
-
-  submitUserSearch(event: Event) {
-    event.preventDefault();
-    const value = this.locationSearch.value;
-
-    this.stateService.searchValue.set(value);
-  }
 }
---
+++ src/app/shared/components/search/search.component.ts
@@ -0,0 +1,27 @@
+import { Component, inject, output } from '@angular/core';
+import { StateService } from '../../../core/services/state.service';
+import { NgxCubeLoaderComponent } from 'ngx-cube-loader';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-search',
+  standalone: true,
+  imports: [NgxCubeLoaderComponent, ReactiveFormsModule, FormsModule],
+  templateUrl: './search.component.html',
+  styleUrl: './search.component.scss',
+})
+export class SearchComponent {
+  readonly stateService = inject(StateService);
+
+  searchValue = output<string>();
+
+  locationSearch = new FormControl('', { nonNullable: true });
+
+  submitUserSearch(event: Event) {
+    event.preventDefault();
+
+    const val = this.locationSearch.value;
+
+    this.searchValue.emit(val);
+  }
+}
